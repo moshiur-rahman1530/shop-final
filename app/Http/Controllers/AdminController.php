60f1c69c2e5d759f33946f54ca7ce58571802d3b
Md. Moshiur Rahman <55827306+moshiur-rahman1530@users.noreply.github.com>
2022-09-26 22:33:16 +0600

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Settings;
 use App\Models\Order;
+use App\Models\Image;
 use App\Models\order_details;
 use Illuminate\Support\Facades\DB;
 use PDF;
@@ -12,6 +13,8 @@ use Mail;
 use Notification;
 use App\Notifications\OrderConfirmNotification;
 use Illuminate\Support\Facades\Auth;
+use File;
+use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
@@ -28,8 +31,6 @@ class AdminController extends Controller
             // 'quote'=>'required|string',
             'short_des'=>'required|string',
             'description'=>'required|string',
-            'photo'=>'required',
-            'logo'=>'required',
             'address1'=>'required|string',
             'address2'=>'required|string',
             'email'=>'required|email',
@@ -45,21 +46,181 @@ class AdminController extends Controller
             'feature3'=>'required|string',
             'icon4'=>'required|string',
             'feature4'=>'required|string',
-            'vendor1'=>'required',
-            'vendor2'=>'required',
-            'vendor3'=>'required',
-            'vendor4'=>'required',
-            'vendor5'=>'required',
-            'vendor6'=>'required',
-            'vendor7'=>'required',
-            'vendor8'=>'required',
 
         ]);
-        $data=$request->all();
-        // dd($data);
-        $settings=Settings::first();
-        $status=$settings->update($data);
-        if($status){
+
+        $firstSetting = Settings::first();
+
+        if (request()->hasFile('logo')){
+
+          $logo = $request->file('logo');
+          $logoSaveAsName = time() . Auth::id() . "-sitelogo." . $logo->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('logo'), $logoSaveAsName);
+          $logoBackupDisk = Storage::disk('backup_google')->url($logoSaveAsName);
+          $logoFilename = Storage::disk('backup_google')->getMetadata($logoSaveAsName);
+          $logoFolderId = $logoFilename['path'];
+          $allImagesUpload = Image::create(['name'=>$logoSaveAsName, 'img_name'=>$logoFolderId, 'path'=>$logoBackupDisk]);
+
+        }else{
+          $logoBackupDisk= $firstSetting->logo;
+        }
+
+        if (request()->hasFile('photo')){
+
+          $photo = $request->file('photo');
+          $photoSaveAsName = time() . Auth::id() . "-sitephoto." . $photo->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('photo'), $photoSaveAsName);
+          $photoBackupDisk = Storage::disk('backup_google')->url($photoSaveAsName);
+          $photoFilename = Storage::disk('backup_google')->getMetadata($photoSaveAsName);
+          $photoFolderId = $photoFilename['path'];
+          $allImagesUpload = Image::create(['name'=>$photoSaveAsName, 'img_name'=>$photoFolderId, 'path'=>$photoBackupDisk]);
+
+        }else{
+          $photoBackupDisk= $firstSetting->photo;
+        }
+
+
+
+        if (request()->hasFile('vendor1')){
+
+          $vendor1 = $request->file('vendor1');
+          $vendor1SaveAsName = time() . Auth::id() . "-vendor." . $vendor1->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('vendor1'), $vendor1SaveAsName);
+          $vendor1BackupDisk = Storage::disk('backup_google')->url($vendor1SaveAsName);
+          $vendor1Filename = Storage::disk('backup_google')->getMetadata($vendor1SaveAsName);
+          $vendor1FolderId = $vendor1Filename['path'];
+          $allImagesUpload = Image::create(['name'=>$vendor1SaveAsName, 'img_name'=>$vendor1FolderId, 'path'=>$vendor1BackupDisk]);
+
+        }else{
+          $vendor1BackupDisk= $firstSetting->vendor1;
+        }
+
+        if (request()->hasFile('vendor2')){
+
+          $vendor2 = $request->file('vendor2');
+          $vendor2SaveAsName = time() . Auth::id() . "-vendor." . $vendor2->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('vendor2'), $vendor2SaveAsName);
+          $vendor2BackupDisk = Storage::disk('backup_google')->url($vendor2SaveAsName);
+          $vendor2Filename = Storage::disk('backup_google')->getMetadata($vendor2SaveAsName);
+          $vendor2FolderId = $vendor2Filename['path'];
+          $allImagesUpload = Image::create(['name'=>$vendor2SaveAsName, 'img_name'=>$vendor2FolderId, 'path'=>$vendor2BackupDisk]);
+
+        }else{
+          $vendor2BackupDisk= $firstSetting->vendor2;
+        }
+        if (request()->hasFile('vendor3')){
+
+          $vendor3 = $request->file('vendor3');
+          $vendor3SaveAsName = time() . Auth::id() . "-vendor." . $vendor3->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('vendor3'), $vendor3SaveAsName);
+          $vendor3BackupDisk = Storage::disk('backup_google')->url($vendor3SaveAsName);
+          $vendor3Filename = Storage::disk('backup_google')->getMetadata($vendor3SaveAsName);
+          $vendor3FolderId = $vendor3Filename['path'];
+          $allImagesUpload = Image::create(['name'=>$vendor3SaveAsName, 'img_name'=>$vendor3FolderId, 'path'=>$vendor3BackupDisk]);
+
+        }else{
+          $vendor3BackupDisk= $firstSetting->vendor3;
+        }
+        if (request()->hasFile('vendor4')){
+
+          $vendor4 = $request->file('vendor4');
+          $vendor4SaveAsName = time() . Auth::id() . "-vendor." . $vendor4->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('vendor4'), $vendor4SaveAsName);
+          $vendor4BackupDisk = Storage::disk('backup_google')->url($vendor4SaveAsName);
+          $vendor4Filename = Storage::disk('backup_google')->getMetadata($vendor4SaveAsName);
+          $vendor4FolderId = $vendor4Filename['path'];
+          $allImagesUpload = Image::create(['name'=>$vendor4SaveAsName, 'img_name'=>$vendor4FolderId, 'path'=>$vendor4BackupDisk]);
+
+        }else{
+          $vendor4BackupDisk= $firstSetting->vendor4;
+        }
+        if (request()->hasFile('vendor5')){
+
+          $vendor5 = $request->file('vendor5');
+          $vendor5SaveAsName = time() . Auth::id() . "-vendor." . $vendor5->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('vendor5'), $vendor5SaveAsName);
+          $vendor5BackupDisk = Storage::disk('backup_google')->url($vendor5SaveAsName);
+          $vendor5Filename = Storage::disk('backup_google')->getMetadata($vendor5SaveAsName);
+          $vendor5FolderId = $vendor5Filename['path'];
+          $allImagesUpload = Image::create(['name'=>$vendor5SaveAsName, 'img_name'=>$vendor5FolderId, 'path'=>$vendor5BackupDisk]);
+
+        }else{
+          $vendor5BackupDisk= $firstSetting->vendor5;
+        }
+        if (request()->hasFile('vendor6')){
+
+          $vendor6 = $request->file('vendor6');
+          $vendor6SaveAsName = time() . Auth::id() . "-vendor." . $vendor6->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('vendor6'), $vendor6SaveAsName);
+          $vendor6BackupDisk = Storage::disk('backup_google')->url($vendor6SaveAsName);
+          $vendor6Filename = Storage::disk('backup_google')->getMetadata($vendor6SaveAsName);
+          $vendor6FolderId = $vendor6Filename['path'];
+          $allImagesUpload = Image::create(['name'=>$vendor6SaveAsName, 'img_name'=>$vendor6FolderId, 'path'=>$vendor6BackupDisk]);
+
+        }else{
+          $vendor6BackupDisk= $firstSetting->vendor6;
+        }
+        if (request()->hasFile('vendor7')){
+          $vendor7 = $request->file('vendor7');
+          $vendor7SaveAsName = time() . Auth::id() . "-vendor." . $vendor7->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('vendor7'), $vendor7SaveAsName);
+          $vendor7BackupDisk = Storage::disk('backup_google')->url($vendor7SaveAsName);
+          $vendor7Filename = Storage::disk('backup_google')->getMetadata($vendor7SaveAsName);
+          $vendor7FolderId = $vendor7Filename['path'];
+          $allImagesUpload = Image::create(['name'=>$vendor7SaveAsName, 'img_name'=>$vendor7FolderId, 'path'=>$vendor7BackupDisk]);
+
+        }else{
+          $vendor7BackupDisk= $firstSetting->vendor7;
+        }
+        if (request()->hasFile('vendor8')){
+
+          $vendor8 = $request->file('vendor8');
+          $vendor8SaveAsName = time() . Auth::id() . "-vendor." . $vendor8->getClientOriginalExtension();
+          Storage::disk('backup_google')->putFileAs("",$request->file('vendor8'), $vendor8SaveAsName);
+          $vendor8BackupDisk = Storage::disk('backup_google')->url($vendor8SaveAsName);
+          $vendor8Filename = Storage::disk('backup_google')->getMetadata($vendor8SaveAsName);
+          $vendor8FolderId = $vendor8Filename['path'];
+          $allImagesUpload = Image::create(['name'=>$vendor8SaveAsName, 'img_name'=>$vendor8FolderId, 'path'=>$vendor8BackupDisk]);
+
+        }else{
+          $vendor8BackupDisk= $firstSetting->vendor8;
+        }
+
+        // $data=$request->all();
+        // $settings=Settings::first();
+        // $status=$settings->update($data);
+
+        $result= Settings::first()->update([
+          'title_first_letter'=>$request->title_first_letter,
+          'title_remain'=>$request->title_remain,
+          'description'=>$request->description,
+          'short_des'=>$request->short_des,
+          'logo'=>$logoBackupDisk,
+          'photo'=>$photoBackupDisk,
+          'address1'=>$request->address1,
+          'address2'=>$request->address2,
+          'phone'=>$request->phone,
+          'email'=>$request->email,
+          'icon1'=>$request->icon1,
+          'icon2'=>$request->icon2,
+          'icon3'=>$request->icon3,
+          'icon4'=>$request->icon4,
+          'feature1'=>$request->feature1,
+          'feature2'=>$request->feature2,
+          'feature3'=>$request->feature3,
+          'feature4'=>$request->feature4,
+          'vendor1'=>$vendor1BackupDisk,
+          'vendor2'=>$vendor2BackupDisk,
+          'vendor3'=>$vendor3BackupDisk,
+          'vendor4'=>$vendor4BackupDisk,
+          'vendor5'=>$vendor5BackupDisk,
+          'vendor6'=>$vendor6BackupDisk,
+          'vendor7'=>$vendor7BackupDisk,
+          'vendor8'=>$vendor8BackupDisk,
+        ]);
+
+
+        if($result){
             return 1;
         }
         else{
